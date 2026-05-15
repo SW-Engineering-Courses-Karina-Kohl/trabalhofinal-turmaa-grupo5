e600138c96bbfe67a1c077e3b5db4012fff45d59
p-fan <p-fantin@hotmail.com>
2026-05-15 13:51:30 -0300

    chore: remove class Aluno

--- src/main/java/br/edu/ufrgs/controller/ServletMedia.java
+++ src/main/java/br/edu/ufrgs/controller/ServletMedia.java
@@ -1,6 +1,5 @@
 package br.edu.ufrgs.controller;
 
-import br.edu.ufrgs.model.Aluno; // Importação do Model
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,17 +13,10 @@ public class ServletMedia extends HttpServlet {
             throws ServletException, IOException {
         
         try {
-            String nome = request.getParameter("nome");
-            double nota = Double.parseDouble(request.getParameter("nota"));
-
-            // Uso da classe Model
-            Aluno aluno = new Aluno(nome, nota);
-            String mensagem = aluno.getMensagemFinal();
-
-            request.setAttribute("resultado", mensagem);
             
-        } catch (NumberFormatException e) {
-            request.setAttribute("resultado", "Erro: Informe uma nota válida.");
+            
+        } catch (Exception e) {
+            
         }
 
         request.getRequestDispatcher("index.jsp").forward(request, response);
