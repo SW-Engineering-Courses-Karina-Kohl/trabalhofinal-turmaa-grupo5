350803964050cdf218aebe2c649e7cd0400038c4
p-fan <p-fantin@hotmail.com>
2026-06-04 08:49:42 -0300

    test: Add tests for ConfigFileReader

---
+++ src/test/java/br/edu/ufrgs/model/ConfigFileReaderTest.java
@@ -0,0 +1,51 @@
+package br.edu.ufrgs.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConfigFileReaderTest {
+    @Test
+    public void testConfigFileReaderSuccess() {
+        ConfigFileReader configReader = new ConfigFileReader();
+        DiscardParameter discardParameter = configReader.loadDiscardParameter("test/config_file_valid_test.csv"); 
+        
+        String expectedMessage = "DiscardParameter{marginOfSafetyDays=3, discardFactorPercentage=1.0}";
+        String message = discardParameter.toString();
+        assertTrue(message.contains(expectedMessage));
+    }
+
+
+    @Test
+    public void testConfigFileReaderMissingFile() {
+        ConfigFileReader configReader = new ConfigFileReader();
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            configReader.loadDiscardParameter("non_existent_file.csv");
+        });
+        String expectedMessage = "Error reading configuration file";
+        String message = exception.getMessage();
+        assertTrue(message.contains(expectedMessage));
+    }       
+
+    @Test
+    public void testConfigFileReaderEmptyField() {
+        ConfigFileReader configReader = new ConfigFileReader();
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            configReader.loadDiscardParameter("test/config_file_empty_field_test.csv");
+        });
+        System.out.println(exception.getMessage());
+        String expectedMessage = "Value for margem_seguranca_dias cannot be empty";
+        String message = exception.getMessage();
+        assertTrue(message.contains(expectedMessage));
+    }
+
+    @Test
+    public void testConfigFileReaderInvalidContent() {
+        ConfigFileReader configReader = new ConfigFileReader();
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            configReader.loadDiscardParameter("test/config_file_invalid_field_test.csv");
+        });
+        System.out.println(exception.getMessage());
+        String expectedMessage = "Value for fator_descarte_percentual cannot be empty or negative";
+        String message = exception.getMessage();
+        assertTrue(message.contains(expectedMessage));
+    }
+}
